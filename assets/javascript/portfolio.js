15736b5be6e9c48bf85df7995063a52ee8e6fa92
V2KMarr <69081091+V2KMarr@users.noreply.github.com>
2021-02-15 22:29:29 -0500

    Add files via upload

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,169 @@
+/*
+
+    - Steam (emoticon: https://steamcommunity-a.akamaihd.net/economy/emoticon/mgh_17)
+    - Tumblr images/icons:
+        a) https://68.media.tumblr.com/730ba51e7f6b0203e023deeb0db8367b/tumblr_osgbbiLXWV1suieauo1_500.jpg
+
+
+GitHub: https://github.com/Lumm1t/yxlrd
+
+*/
+
+'use strict';
+
+const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c7b5ef89f65c3faa1a';
+
+const timeouts = [];
+
+const mobileAndTabletCheck = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+
+$('html').on('contextmenu', (event) => {
+  const img = document.createElement('img');
+
+  const trollfaceLight = app.skippedIntro ? '' : 'trollface-light';
+
+  img.src = 'assets/others/trollface.jpg';
+  img.width = 32;
+  img.height = 32;
+  img.alt = 'cleanboi$';
+  img.style = `position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; z-index: 10`;
+  img.className = `troll ${trollfaceLight}`;
+
+  document.body.appendChild(img);
+});
+
+setInterval(() => {
+  $('.troll').remove();
+}, 600);
+
+$('.skip').click(() => {
+  skipIntro();
+});
+
+$.fn.extend({
+  animateCss: function (animationName) {
+    const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    this.addClass(`animated ${animationName}`).one(animationEnd, () => {
+      $(this).removeClass(`animated ${animationName}`);
+    });
+
+    return this;
+  },
+});
+
+const writeLine = (text, speed, timeout, callback) => {
+  timeout = typeof timeout === 'number' ? timeout : [0, (callback = timeout)];
+
+  const lineNumber = app.id !== 2 ? ++app.id : (app.id += 2);
+
+  setTimeout(() => {
+    const typed = new Typed(`#line${lineNumber}`, {
+      strings: text,
+      typeSpeed: speed,
+      onComplete: callback,
+    });
+  }, timeout);
+};
+
+$.getJSON(ipgeolocation, (data) => {
+  writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[unknown]</span>..."], 30, () => {
+    if (app.skippedIntro) return;
+
+    clearCursor();
+
+    const usernames = ['user', 'dude'];
+
+    const ip = data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)];
+    const country = data.country_name ? data.country_name : 'your country';
+
+    writeLine([`Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`, `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from ${country} here!`], 30, 500, () => {
+      if (app.skippedIntro) return;
+
+      clearCursor();
+
+      writeLine([`<i style='color: #F62459'>cleanboi$$$</i>`], 120, 500, () => {
+        timeouts.push(
+          setTimeout(() => {
+            if (app.skippedIntro) return;
+
+            clearCursor();
+
+            setTimeout(() => {
+              skipIntro();
+            }, 500);
+          }, 1000)
+        );
+      });
+    });
+  });
+});
+
+const skipIntro = () => {
+  if (app.skippedIntro) return;
+
+  app.skippedIntro = true;
+
+  timeouts.forEach((timeout) => {
+    clearTimeout(timeout);
+  });
+
+  $('.top-right').remove();
+
+  $('#main').fadeOut(100, () => {
+    $('#main').remove();
+
+    $('#marquee').marquee({
+      duration: 15000,
+      gap: 420,
+      delayBeforeStart: 1000,
+      direction: 'left',
+      duplicated: true,
+    });
+
+    setTimeout(() => {
+      $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
+    }, 200);
+
+    setTimeout(() => {
+      const typed = new Typed('#brand', {
+        strings: app.brandDescription,
+        typeSpeed: 40,
+
+        onComplete: () => {
+          clearCursor();
+        },
+      });
+    }, 1350);
+
+    setTimeout(() => {
+      if (!app.shouldIgnoreVideo) {
+        app.videoElement.play();
+        app.audioElement.play();
+      }
+
+      app.videoElement.addEventListener(
+        'timeupdate',
+        () => {
+          $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
+        },
+        false
+      );
+
+      $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
+
+      $('.marquee-container').animateCss('zoomIn');
+
+      $('.container').fadeIn();
+
+      $('.background').fadeIn(200, () => {
+        if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
+      });
+    }, 200);
+  });
+};
+
+const clearCursor = () => {
+  return $('span').siblings('.typed-cursor').css('opacity', '0');
+};
